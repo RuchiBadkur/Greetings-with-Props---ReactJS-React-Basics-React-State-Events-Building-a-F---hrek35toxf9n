736fbc3f2fb4b3ea45918b270af5be88d1293929
Ruchi Badkur <94377483+RuchiBadkur@users.noreply.github.com>
2022-08-24 12:27:22 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,18 @@
 // write code for Welcome component here
+import React from 'react';
+const Welcome = (props) => {
+  return (
+    <>
+    <h1>Hey ! {props.name}</h1>
+    <h2>Welcome to Newton School.</h2>
+    </>
+    )
+}
+export default Welcome;
+// "Hey !" where name is name of the student using h1 tag.
+
+// Also render the "Welcome to Newton School." using h2 tag.
+
+// A prop named "name" will be passed to Welcome component use it in writing greeting text.
+
+
